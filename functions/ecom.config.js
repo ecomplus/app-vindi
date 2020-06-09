c2c048a35f7e75083deaf451b8229d68db005ebd
Leonardo Matos <leomp120894@gmail.com>
2020-06-09 01:18:53 -0300

    chore: update pkg/app title and description

--- functions/ecom.config.js
+++ functions/ecom.config.js
@@ -7,8 +7,8 @@
 
 const app = {
   app_id: 9000,
-  title: 'My Awesome E-Com Plus App',
-  slug: 'my-awesome-app',
+  title: 'Vindi',
+  slug: 'vindi',
   type: 'external',
   state: 'active',
   authentication: true,
