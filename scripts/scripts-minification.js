f5418fef2ccb4d53dd516512fd86ccd1c7de06d9
Leonardo Matos <leomp120894@gmail.com>
2020-07-29 19:35:18 -0300

    build(scripts-minification): debug on minify error (js syntax)

--- scripts/scripts-minification.js
+++ scripts/scripts-minification.js
@@ -7,8 +7,12 @@ fs.readdirSync(publicPath).forEach(file => {
   if (file.endsWith('.js') && !file.endsWith('.min.js')) {
     const filePath = path.resolve(publicPath, file)
     if (filePath) {
-      const jsContent = uglifyJS.minify(fs.readFileSync(filePath, 'utf8')).code
-      fs.writeFileSync(filePath.replace(/.js$/, '.min.js'), jsContent, 'utf8')
+      const minify = uglifyJS.minify(fs.readFileSync(filePath, 'utf8'))
+      if (minify.code) {
+        fs.writeFileSync(filePath.replace(/.js$/, '.min.js'), minify.code, 'utf8')
+      } else {
+        console.error(minify)
+      }
     }
   }
 })
