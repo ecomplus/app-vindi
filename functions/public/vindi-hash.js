594face58e5d6b71d5289fd946fea5ff41c950a6
Leonardo Matos <leomp120894@gmail.com>
2020-07-29 18:54:24 -0300

    feat(js-client): function to create vindi payment profile on client

---
+++ functions/public/vindi-hash.js
@@ -0,0 +1,27 @@
+;(function () {
+  window._vindiHash = function (card) {
+    return new Promise(function (resolve, reject) {
+      // https://atendimento.vindi.com.br/hc/pt-br/articles/115009609107-Como-eu-cadastro-perfis-de-pagamento-
+      window.axios({
+        method: 'post',
+        url: 'https://app.vindi.com.br/api/v1/public/payment_profiles',
+        headers: {
+          Authorization: 'Basic ' + window.btoa(window._vindiKey + ':')
+        },
+        data: {
+          holder_name: card.name,
+          card_expiration: card.month.padStart(2, '0') + '/' + card.year.toString().padStart(4, '20'),
+          card_number: card.number,
+          card_cvv: card.cvc,
+          payment_method_code: 'credit_card',
+          payment_company_code: card.brand
+        },
+        responseType: 'json'
+      })
+        .then(data => {
+          resolve(data.payment_profile.gateway_token)
+        })
+        .catch(reject)
+    })
+  }
+}())
