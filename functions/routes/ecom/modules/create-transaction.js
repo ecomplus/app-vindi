4562223aaffdf389ab8552175c4957de04e9f604
Leonardo Matos <leomp120894@gmail.com>
2020-08-05 00:44:39 -0300

    fix(create-transaction): try setting bill item 'product_code' (string)

--- functions/routes/ecom/modules/create-transaction.js
+++ functions/routes/ecom/modules/create-transaction.js
@@ -126,7 +126,7 @@ exports.post = ({ appSdk, admin }, req, res) => {
       garanta que todos sejam mutuamente válidos"
       */
       vindiBill.bill_items = [{
-        product_code: params.order_number,
+        product_code: String(params.order_number),
         amount: finalAmount
       }]
 
