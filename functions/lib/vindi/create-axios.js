28cfde630bd66ac42c4277ce9a914e649dc69f62
Leonardo Matos <leomp120894@gmail.com>
2020-08-05 00:54:59 -0300

    test(vindi): using sandbox api

--- functions/lib/vindi/create-axios.js
+++ functions/lib/vindi/create-axios.js
@@ -3,7 +3,7 @@ const axios = require('axios')
 module.exports = vindiApiKey => {
   // https://vindi.github.io/api-docs/dist/
   return axios.create({
-    baseURL: 'https://app.vindi.com.br/api/v1/',
+    baseURL: 'https://sandbox-app.vindi.com.br/api/v1/',
     headers: {
       Authorization: 'Basic ' + Buffer.from(`${vindiApiKey}:`).toString('base64')
     }
