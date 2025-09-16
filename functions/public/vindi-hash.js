b60a806b14f1140981472187ffd8c4ab19ea7bcd
Leonardo Matos <leomp120894@gmail.com>
2025-09-16 01:22:18 -0300

    fix: update vindi hash script to use native browser fetch in place of axios

--- functions/public/vindi-hash.js
+++ functions/public/vindi-hash.js
@@ -1,28 +1,35 @@
+/* eslint-disable */
 ;(function () {
   window._vindiHash = function (card) {
     return new Promise(function (resolve, reject) {
       // https://atendimento.vindi.com.br/hc/pt-br/articles/115009609107-Como-eu-cadastro-perfis-de-pagamento-
       var vindiSubdomain = window._vindiSandbox ? 'sandbox-app' : 'app'
-      window.axios({
-        method: 'post',
-        url: 'https://' + vindiSubdomain + '.vindi.com.br/api/v1/public/payment_profiles',
+      fetch('https://' + vindiSubdomain + '.vindi.com.br/api/v1/public/payment_profiles', {
+        method: 'POST',
         headers: {
-          Authorization: 'Basic ' + window.btoa(window._vindiKey + ':')
+          'Authorization': 'Basic ' + window.btoa(window._vindiKey + ':'),
+          'Content-Type': 'application/json'
         },
-        data: {
+        body: JSON.stringify({
           holder_name: card.name,
           card_expiration: card.month.padStart(2, '0') + '/' + card.year.toString().padStart(4, '20'),
           card_number: card.number,
           card_cvv: card.cvc,
           payment_method_code: 'credit_card',
           payment_company_code: card.brand
-        },
-        responseType: 'json'
+        })
       })
         .then(function (response) {
-          resolve(response.data.payment_profile.gateway_token)
+          if (!response.ok) {
+            throw new Error('Card hash request failed with ' + response.status)
+          }
+          return response.json()
+        })
+        .then(function (data) {
+          resolve(data.payment_profile.gateway_token)
         })
         .catch(reject)
     })
   }
 }())
+
